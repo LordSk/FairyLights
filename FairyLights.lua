52ff2448ae8bad08efab89e796c88f611759c5af
LordSk <lordskelethom@gmail.com>
2014-12-08 18:19:16 +0100

    Light patterns

--- FairyLights.lua
+++ FairyLights.lua
@@ -1,5 +1,15 @@
+function clamp(val, min , max)
+	if val < min then
+		return min
+	elseif val > max then
+		return max
+	end
+	return val
+end
+
 function Initialize()
-	lightSwitch = false
+	math.randomseed(os.time())
+	
 	lightsOff = {}
 	lightsOn = {}
 	lightNum = 13
@@ -11,19 +21,56 @@ function Initialize()
 		lightsOn[#lightsOn + 1] = SKIN:GetMeter('MeterLightOn'..i)
 		lightsOn[#lightsOn]:Hide()
 	end
+	
+	-- Light patterns
+	patterns = {}
+	patterns[1] = {
+		{1,1,1,1,1,1,1,1,1,1,1,1,1},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{1,1,1,1,1,1,1,1,1,1,1,1,1},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0},
+	}
+	patterns[2] = {
+		{0,1,0,1,0,1,0,1,0,1,0,1,0},
+		{1,0,1,0,1,0,1,0,1,0,1,0,1},
+		{0,1,0,1,0,1,0,1,0,1,0,1,0},
+		{1,0,1,0,1,0,1,0,1,0,1,0,1},
+	}
+	patterns[3] = {
+		{1,1,0,0,0,0,0,0,0,0,0,1,1},
+		{1,1,1,1,0,0,0,0,0,1,1,1,1},
+		{1,1,1,1,1,1,0,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1,1},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0},
+	}
+	
+	patternCur = 1
+	lightPatternOptn = SELF:GetNumberOption('LightPattern', 0)
+	pattChosen = clamp(lightPatternOptn, 1, #patterns)
+	
+	-- random pattern
+	if lightPatternOptn == 0 then
+		pattChosen = math.random(1, #patterns)
+	end
 end
 
 function Update()
-	lightSwitch = not lightSwitch
-	
 	for i = 1, lightNum do
-		if lightSwitch then
+		if patterns[pattChosen][patternCur][i] == 1 then
 			lightsOn[i]:Show()
 			lightsOff[i]:Hide()
 		else
 			lightsOn[i]:Hide()
 			lightsOff[i]:Show()
 		end
+	end
+	
+	-- Current pattern ended
+	if lightPatternOptn == 0 and patternCur == #patterns[pattChosen] then
+		pattChosen = math.random(1, #patterns)
+		patternCur = 1
+	else
+		patternCur = patternCur%#patterns[pattChosen] + 1
 	end
 end
 
